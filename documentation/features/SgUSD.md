c87e460ff57ff31d087f0110641b415952fee584
walk-on-me <rabifromthenight@gmail.com>
2025-01-21 10:44:54 +0100

    Add restrictions on the TgUSD bridge

--- documentation/features/SgUSD.md
+++ documentation/features/SgUSD.md
@@ -30,11 +30,11 @@ Chainview used to get global infos of the page is **SgUSDUI** and is called with
 - She allows `sgUSD` to spend her `tgUSD`. ( **approve(sgUSDAddress,amount)** on `tgUSD` )
 - She inputs 1000 `tgUSD`.
   - A call is made to `sgUSD` to return the amount of sgUSD minted in return ( **previewDeposit(tgUSDAmount)** ).
-- She deposits `tgUSD` on `sgUSD` and she received `sgUSD`. ( **deposit(tgUSDAmount,userAddress)** on `sgUSD` )
+- She deposits `tgUSD` on `sgUSD` and she received `sgUSD`. ( **deposit(tgUSDAmount, userAddress)** on `sgUSD` )
 
 ## Withdraw
 
 - Alice owns 1000 `sgUSD` from staking tgUSD.
 - She inputs 1000 `sgUSD`.
   - A call is made to `sgUSD` to return the amount of sgUSD minted in return ( **previewRedeem(sgUSDAmount)** ).
-- She withdraws `tgUSD` and get her `sgUSD` burnt. ( **deposit(tgUSDAmount,userAddress, userAddress)** on `sgUSD` )
+- She withdraws `tgUSD` and get her `sgUSD` burnt. ( **redeem(tgUSDAmount, userAddress, userAddress)** on `sgUSD` )
--- src/tgUSD/tokens/TgUSD.sol
+++ src/tgUSD/tokens/TgUSD.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.22;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import {SendParam, OFTReceipt} from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
+import {MessagingFee, MessagingReceipt} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {ITgUSD} from "../../interfaces/internals/tgUSD/ITgUSD.sol";
 import {IDebtIR} from "../../interfaces/internals/tgUSD/IDebtIR.sol";
 import {IControlTower} from "../../interfaces/internals/tgUSD/IControlTower.sol";
-
+import {IBridgeChecker} from "../../interfaces/internals/tgUSD/IBridgeChecker.sol";
 import "forge-std/console.sol";
 /// @notice OFT is an ERC-20 token that extends the OFTCore contract.
 contract TgUSD is OFT, ITgUSD {
@@ -14,7 +16,13 @@ contract TgUSD is OFT, ITgUSD {
 
     uint256 public mintableInterests;
 
+    bool public isBridgePermisionless;
+
+    IBridgeChecker public bridgeChecker;
+
     error CallerNotMinterBurner();
+    error OnlyOwnerCanBridgeIfPermisionlessNotActive();
+    error BridgingNotAllowed();
 
     constructor(
         string memory _name,
@@ -51,5 +59,52 @@ contract TgUSD is OFT, ITgUSD {
 
     function increaseMintableInterests(uint256 interests) external onlyMarketCaller {
         mintableInterests += interests;
+    }
+
+    /**
+     * @dev Executes the send operation.
+     * @param _sendParam The parameters for the send operation.
+     * @param _fee The calculated fee for the send() operation.
+     *      - nativeFee: The native fee.
+     *      - lzTokenFee: The lzToken fee.
+     * @param _refundAddress The address to receive any excess funds.
+     * @return msgReceipt The receipt for the send operation.
+     * @return oftReceipt The OFT receipt information.
+     *
+     * @dev MessagingReceipt: LayerZero msg receipt
+     *  - guid: The unique identifier for the sent message.
+     *  - nonce: The nonce of the sent message.
+     *  - fee: The LayerZero fee incurred for the message.
+     */
+    function send(
+        SendParam calldata _sendParam,
+        MessagingFee calldata _fee,
+        address _refundAddress
+    ) external payable override returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) {
+        // When the bridge is restricted to the DAO, only the owner of the contract can bridge some tgUSD
+        if (!isBridgePermisionless) {
+            require(msg.sender == owner(), OnlyOwnerCanBridgeIfPermisionlessNotActive());
+        }
+        // When the bridge is setup as permisionless, it needs to match some conditions given by the bridge checker contract.
+        else {
+            require(
+                msg.sender == owner || bridgeChecker.isBridgingAllowed(msg.sender, _refundAddress, _sendParam.amountLD, _sendParam.minAmountLD, _sendParam.dstEid),
+                BridgingNotAllowed()
+            );
+        }
+        // Applies the token transfers regarding this send() operation.
+        // - amountSentLD is the amount in local decimals that was ACTUALLY sent/debited from the sender.
+        // - amountReceivedLD is the amount in local decimals that will be received/credited to the recipient on the remote OFT instance.
+        (uint256 amountSentLD, uint256 amountReceivedLD) = _debit(msg.sender, _sendParam.amountLD, _sendParam.minAmountLD, _sendParam.dstEid);
+
+        // Builds the options and OFT message to quote in the endpoint.
+        (bytes memory message, bytes memory options) = _buildMsgAndOptions(_sendParam, amountReceivedLD);
+
+        // Sends the message to the LayerZero endpoint and returns the LayerZero msg receipt.
+        msgReceipt = _lzSend(_sendParam.dstEid, message, options, _fee, _refundAddress);
+        // Formulate the OFT receipt.
+        oftReceipt = OFTReceipt(amountSentLD, amountReceivedLD);
+
+        emit OFTSent(msgReceipt.guid, _sendParam.dstEid, msg.sender, amountSentLD, amountReceivedLD);
     }
 }
