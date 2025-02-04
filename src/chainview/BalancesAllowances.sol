a89e9a0ca8ed4ef3a7b233f8c26285bf7ceaf1c0
walk-on-me <rabifromthenight@gmail.com>
2025-02-04 16:32:00 +0100

    Add GetBalancesAllowances chainview

--- src/chainview/BalancesAllowances.sol
+++ src/chainview/BalancesAllowances.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract BalancesAllowances {
+abstract contract BalancesAllowances {
     struct InputBalancesAllowances {
         IERC20 token;
         address[] spenders;
