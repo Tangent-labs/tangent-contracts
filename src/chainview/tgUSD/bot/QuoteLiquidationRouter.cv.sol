d7242279bc9ba93469650a000baf6cd2eb5df765
Trottoir <31350013+Trottoir@users.noreply.github.com>
2025-04-08 11:22:12 +0200

    Remove WStable struct from QuoteLiquidationChainview (#78)

--- src/chainview/tgUSD/bot/QuoteLiquidationRouter.cv.sol
+++ src/chainview/tgUSD/bot/QuoteLiquidationRouter.cv.sol
@@ -11,36 +11,19 @@ struct CurveQuote {
     address[5] _pools;
 }
 
-struct WStableQuote {
-    address stablePool;
-    int128 i;
-    int128 j;
-}
-struct QuoteLiquidationRouterIn {
-    CurveQuote curveQuote;
-    WStableQuote wStableQuote;
-}
-
 contract QuoteLiquidationRouter {
     ICurveRouter public constant CURVE_ROUTER = ICurveRouter(0x16C6521Dff6baB339122a0FE25a9116693265353);
 
     error QuoteLiquidationRouterError(uint256[] quotes);
 
-    constructor(QuoteLiquidationRouterIn[] memory routes) {
+    constructor(CurveQuote[] memory routes) {
         uint256 routesLen = routes.length;
         uint256[] memory quotes = new uint256[](routesLen);
 
         for (uint256 i; i < routesLen; ) {
-            CurveQuote memory curveQuote = routes[i].curveQuote;
-            WStableQuote memory wStableQuote = routes[i].wStableQuote;
+            CurveQuote memory curveQuote = routes[i];
 
-            uint256 amountReceived = CURVE_ROUTER.get_dy(curveQuote._route, curveQuote._swap_params, curveQuote._amount, curveQuote._pools);
-
-            if (address(wStableQuote.stablePool) != address(0)) {
-                amountReceived = ICurveStableSwapNG(wStableQuote.stablePool).get_dy(wStableQuote.i, wStableQuote.j, amountReceived);
-            }
-
-            quotes[i] = amountReceived;
+            quotes[i] = CURVE_ROUTER.get_dy(curveQuote._route, curveQuote._swap_params, curveQuote._amount, curveQuote._pools);
             unchecked {
                 ++i;
             }
--- test/tgUSD/chainview/QuoteLiquidationRouterChainview.t.sol
+++ test/tgUSD/chainview/QuoteLiquidationRouterChainview.t.sol
@@ -6,14 +6,14 @@ import "../handler/Features/BorrowRepay/HBorrow.sol";
 
 import "../handler/Features/BorrowRepay/HBorrow.sol";
 
-import {QuoteLiquidationRouter, CurveQuote, WStableQuote, QuoteLiquidationRouterIn} from "../../../src/chainview/tgUSD/bot/QuoteLiquidationRouter.cv.sol";
+import {QuoteLiquidationRouter, CurveQuote} from "../../../src/chainview/tgUSD/bot/QuoteLiquidationRouter.cv.sol";
 
 contract QuoteLiquidationRouterChainview is ConvexCurveContext {
     uint256 constant ZERO = 0;
 
     // QUOTE crvUSD-USDC => USDC => tgUSD-USDC => tgUSD
     function test_quote_curve_router_chainview_without_wStable() public {
-        QuoteLiquidationRouterIn[] memory quoteIn = new QuoteLiquidationRouterIn[](1);
+        CurveQuote[] memory curveQuotes = new CurveQuote[](1);
         address[11] memory route = [
             address(AddrCurveStableLP.CRVUSD_USDC),
             address(AddrCurveStableLP.CRVUSD_USDC),
@@ -36,17 +36,16 @@ contract QuoteLiquidationRouterChainview is ConvexCurveContext {
         ];
         address[5] memory pools = [address(0), address(0), address(0), address(0), address(0)];
 
-        CurveQuote memory curveQuote = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
-        WStableQuote memory wStableQuote = WStableQuote({stablePool: address(0), i: int128(0), j: int128(1)});
-
-        quoteIn[0] = QuoteLiquidationRouterIn({curveQuote: curveQuote, wStableQuote: wStableQuote});
-        try new QuoteLiquidationRouter(quoteIn) {} catch (bytes memory reason) {
+        curveQuotes[0] = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
+        try new QuoteLiquidationRouter(curveQuotes) {} catch (bytes memory reason) {
             assertTrue(reason.length > 3, "Chainview failed");
         }
     }
 
     // QUOTE crvUSD-USDC => crvUSD => wcrvUSD => tgUSD-wcrvUSD => tgUSD
     function test_liquidator_curve_router_chainview_with_wStable() public {
+        CurveQuote[] memory curveQuotes = new CurveQuote[](1);
+
         address[11] memory route = [
             address(AddrCurveStableLP.CRVUSD_USDC),
             address(AddrCurveStableLP.CRVUSD_USDC),
@@ -69,17 +68,15 @@ contract QuoteLiquidationRouterChainview is ConvexCurveContext {
         ];
         address[5] memory pools = [address(0), address(0), address(0), address(0), address(0)];
 
-        CurveQuote memory curveQuote = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
-        WStableQuote memory wStableQuote = WStableQuote({stablePool: address(lpDeploymentContext.tgUSDLPs("tgUSD-wcrvUSD")), i: int128(0), j: int128(1)});
-
-        QuoteLiquidationRouterIn[] memory quoteIn = new QuoteLiquidationRouterIn[](1);
-        quoteIn[0] = QuoteLiquidationRouterIn({curveQuote: curveQuote, wStableQuote: wStableQuote});
-        try new QuoteLiquidationRouter(quoteIn) {} catch (bytes memory reason) {
+        curveQuotes[0] = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
+        try new QuoteLiquidationRouter(curveQuotes) {} catch (bytes memory reason) {
             assertTrue(reason.length > 3, "Chainview failed");
         }
     }
 
     function test_liquidator_curve_router_chainview_multi_quote() public {
+        CurveQuote[] memory curveQuotes = new CurveQuote[](2);
+
         address[11] memory route = [
             address(AddrCurveStableLP.CRVUSD_USDC),
             address(AddrCurveStableLP.CRVUSD_USDC),
@@ -103,7 +100,6 @@ contract QuoteLiquidationRouterChainview is ConvexCurveContext {
         address[5] memory pools = [address(0), address(0), address(0), address(0), address(0)];
 
         CurveQuote memory curveQuote1 = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
-        WStableQuote memory wStableQuote1 = WStableQuote({stablePool: address(0), i: int128(0), j: int128(1)});
 
         route = [
             address(AddrCurveStableLP.CRVUSD_USDC),
@@ -128,12 +124,10 @@ contract QuoteLiquidationRouterChainview is ConvexCurveContext {
         pools = [address(0), address(0), address(0), address(0), address(0)];
 
         CurveQuote memory curveQuote2 = CurveQuote({_route: route, _swap_params: swapParams, _amount: 100 ether, _pools: pools});
-        WStableQuote memory wStableQuote2 = WStableQuote({stablePool: address(lpDeploymentContext.tgUSDLPs("tgUSD-wfrxUSD")), i: int128(0), j: int128(1)});
 
-        QuoteLiquidationRouterIn[] memory quoteIn = new QuoteLiquidationRouterIn[](2);
-        quoteIn[0] = QuoteLiquidationRouterIn({curveQuote: curveQuote1, wStableQuote: wStableQuote1});
-        quoteIn[1] = QuoteLiquidationRouterIn({curveQuote: curveQuote2, wStableQuote: wStableQuote2});
-        try new QuoteLiquidationRouter(quoteIn) {} catch (bytes memory reason) {
+        curveQuotes[0] = curveQuote1;
+        curveQuotes[1] = curveQuote2;
+        try new QuoteLiquidationRouter(curveQuotes) {} catch (bytes memory reason) {
             uint256[] memory results = abi.decode(removeFirst4Bytes(reason), (uint256[]));
             assertGt(results[0], 100 ether);
             assertGt(results[1], 100 ether);
