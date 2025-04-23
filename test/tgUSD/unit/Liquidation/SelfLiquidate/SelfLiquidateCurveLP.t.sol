289bd35fa24bdba0fbe64f9148dfcd17de85d64c
Trottoir <31350013+Trottoir@users.noreply.github.com>
2025-04-23 12:18:55 +0200

    Add some testing on SelfLiquidation (#82)

--- test/tgUSD/unit/Liquidation/SelfLiquidate/SelfLiquidateCurveLP.t.sol
+++ test/tgUSD/unit/Liquidation/SelfLiquidate/SelfLiquidateCurveLP.t.sol
@@ -18,6 +18,10 @@ contract SelfLiquidateCurveLP is ConvexCurveContext {
     ICurveStableSwapNG public lpTgUSD_wfrxUSD;
 
     uint256[][] public swapParams;
+    address[] public route;
+
+    uint256 public collatDeposited = 5_000 ether;
+    uint256 public initialDebt = 4_250 ether;
     function setUp() public {
         collatToken = AddrCurveStableLP.CRVUSD_USDC;
         lpTgUSD_USDC = lpDeploymentContext.tgUSDLPs("tgUSD-USDC");
@@ -35,14 +39,17 @@ contract SelfLiquidateCurveLP is ConvexCurveContext {
         uint256[] memory swapUsdcToTgUSD = Array.memoryUint256([zero, uint256(1), uint256(1), uint256(10), uint256(2)]);
         swapParams.push(unwrapLPToUSDC);
         swapParams.push(swapUsdcToTgUSD);
+
+        hDeposit.depositAndBorrow(collatDeposited, initialDebt, true, address(0));
+
+        route.push(address(AddrCurveStableLP.CRVUSD_USDC));
+        route.push(address(AddrCurveStableLP.CRVUSD_USDC));
+        route.push(address(AddrClassicERC20.TOKEN_USDC));
+        route.push(address(lpTgUSD_USDC));
+        route.push(address(tgUSD));
     }
 
     function test_selfLiquidate_all_position_curveLP() external {
-        uint256 collatDeposited = 5_000 ether;
-        hDeposit.depositAndBorrow(collatDeposited, 4_250 ether, true, address(0));
-
-        irCalculator.checkpointIR(address(market));
-
         vm.startPrank(usr1);
         uint256 collatToDump = market.collateralBalances(usr1);
 
@@ -51,29 +58,39 @@ contract SelfLiquidateCurveLP is ConvexCurveContext {
             MAX_UINT,
             address(AddrRouter.ROUTER_CURVE),
             4_250 ether,
-            encoder.encodeLiquidateCallForCurveLP(
-                encoder.createCurveRouterStruct(
-                    Array.memoryAddress(
-                        [
-                            address(AddrCurveStableLP.CRVUSD_USDC),
-                            address(AddrCurveStableLP.CRVUSD_USDC),
-                            address(AddrClassicERC20.TOKEN_USDC),
-                            address(lpTgUSD_USDC),
-                            address(tgUSD)
-                        ]
-                    ),
-                    swapParams,
-                    collatToDump,
-                    4_250 ether,
-                    usr1
-                )
-            )
+            encoder.encodeLiquidateCallForCurveLP(encoder.createCurveRouterStruct(route, swapParams, collatToDump, 4_250 ether, usr1))
         );
 
-        // Liquidation passes after IR increased the user debt over the liquidation threshold
-
+        assertEq(market.collateralBalances(usr1), 0);
+        assertEq(market.totalCollateral(), 0);
         assertEq(market.userDebt(usr1), 0);
         assertEq(market.totalDebt(), 0);
+
+        vm.stopPrank();
+    }
+
+    function test_selfLiquidate_partial_position_curveLP() external {
+        vm.startPrank(usr1);
+
+        uint256 amountToRepay = 1_000 ether;
+        uint256 amountToLiquidate = 1_000 ether;
+
+        uint256 surplus = tgUSD.balanceOf(usr1);
+        market.selfLiquidate(
+            amountToLiquidate,
+            amountToRepay,
+            address(AddrRouter.ROUTER_CURVE),
+            0,
+            encoder.encodeLiquidateCallForCurveLP(encoder.createCurveRouterStruct(route, swapParams, amountToLiquidate, 0, usr1))
+        );
+        surplus = tgUSD.balanceOf(usr1) - surplus;
+
+        assertGt(surplus, 10 ether);
+
+        assertEq(market.collateralBalances(usr1), collatDeposited - amountToLiquidate);
+        assertEq(market.totalCollateral(), collatDeposited - amountToLiquidate);
+        assertEq(market.userDebt(usr1), initialDebt - amountToRepay);
+        assertEq(market.totalDebt(), initialDebt - amountToRepay);
 
         vm.stopPrank();
     }
