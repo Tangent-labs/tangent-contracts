0775eb074b02099d2cb328bc3e3e44b17206854b
Trottoir <maxime.ranciere@2018.icam.fr>
2025-05-30 13:56:56 +0200

    Add test on all branches Oracle PT

--- test/unit/WStables/wUSR.t.sol
+++ test/unit/WStables/wUSR.t.sol
@@ -166,7 +166,7 @@ contract wUSR is MarketDeploymentContext {
         wUSR.claimRewards();
     }
 
-    function test_convert_views() external {
+    function test_convert_views() external view {
         uint256 amount = 100;
         assertEq(wUSR.convertToAssets(amount), amount);
         assertEq(wUSR.convertToShares(amount), amount);
