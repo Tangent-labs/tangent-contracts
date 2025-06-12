0ef8930e73d0f129daab8da78b0755ae63c07ae0
yann-j-nomiks <yannj@zohomail.com>
2025-06-12 21:24:59 +0200

    refacto Chain view

--- src/chainview/boosters/LandingBooster.cv.sol
+++ src/chainview/boosters/LandingBooster.cv.sol
@@ -1,24 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-interface ISdtStakingViewer {
-    struct SdAssetGlobalView {
-        address gaugeAsset;
-        address sdAsset;
-        address asset;
-        address stakingAddress;
-        uint256 cvgCycle;
-        uint256 previousTotal;
-        uint256 actualTotal;
-        uint256 nextTotal;
-    }
-}
-
-interface IGlobalViewSdAssetStaking {
-    function getGlobalViewSdAssetStaking(address[] calldata stakingContracts) external view returns (ISdtStakingViewer.SdAssetGlobalView[] memory);
-}
-
-interface ICvxStakingPositionService {
+interface IStakingPositionService {
     struct CycleInfo {
         uint256 cvgRewardsAmount;
         uint256 totalStaked;
@@ -34,34 +17,37 @@ contract LandingBooster {
     }
     error LandingBoosterError(LandingBoosterOut output);
 
-    address public SdtStakingViewer = 0xA3A8cDA21f50b6737385E46FC9495a9998B05Ff0;
     address public cvgCvxstaking = 0x2c1D293c50C6d1a4370ebb442A02c5956bbAb119;
-    address public cvgCVX = 0x2191DF768ad71140F9F3E96c1e4407A4aA31d082;
 
     constructor(address[] memory lpAssets) {
+        // TODO  supply USG , supply sUSG , prix sUSG , [ apy USG  => API ]
+
+        // TODO  apy RSTAN  , supply locked RSTAN , price RSTAN
+
         uint256[] memory boosterTvl = getBoosterTvl(lpAssets);
         LandingBoosterOut memory output = LandingBoosterOut({amounts: boosterTvl});
         revert LandingBoosterError(output);
     }
 
     /*
-     * @notice Get the total TVL of the booster (Stake DAO + Convex)
-     * @return totalTvl The total TVL of the booster
+     * @notice Get the total amount of token  of the booster (Stake DAO + Convex)
+     * @return Array of token amounts of the booster
      */
     function getBoosterTvl(address[] memory lpAssets) public view returns (uint256[] memory) {
         uint256[] memory amounts = new uint256[](lpAssets.length + 1);
 
         // Get the stake DAO part
-        ISdtStakingViewer.SdAssetGlobalView[] memory sdAssetGlobalView = IGlobalViewSdAssetStaking(SdtStakingViewer).getGlobalViewSdAssetStaking(lpAssets);
         uint256 i = 0;
-        for (i; i < sdAssetGlobalView.length; i++) {
-            amounts[i] = sdAssetGlobalView[i].actualTotal;
+        for (i; i < lpAssets.length; i++) {
+            IStakingPositionService sdtService = IStakingPositionService(lpAssets[i]);
+            uint256 cycle = sdtService.stakingCycle();
+            amounts[i] = sdtService.cycleInfo(cycle + 1).totalStaked;
         }
 
         // Get the Convex part
-        ICvxStakingPositionService staking = ICvxStakingPositionService(cvgCvxstaking);
-        uint256 currentCycle = staking.stakingCycle();
-        amounts[i] = staking.cycleInfo(currentCycle + 1).totalStaked;
+        IStakingPositionService cvgCvxPositionService = IStakingPositionService(cvgCvxstaking);
+        uint256 currentCycle = cvgCvxPositionService.stakingCycle();
+        amounts[i] = cvgCvxPositionService.cycleInfo(currentCycle + 1).totalStaked;
 
         return amounts;
     }
