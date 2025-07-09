607f7faa69b2efaf94433100c3899027db0e66ce
Vincent <vincent.granie@2018.icam.fr>
2025-07-09 12:18:44 +0200

    add mapping method

--- js-scripts/hardhat/USG/scripts/campaignActions.ts
+++ js-scripts/hardhat/USG/scripts/campaignActions.ts
@@ -5,12 +5,9 @@ import {withdrawCurveLP} from "../actions/withdrawCurveLP";
 import {withdrawStakeDao} from "../actions/withdrawStakeDao";
 import {depositCurveGauge} from "../actions/depositCurveGauge";
 import {withdrawCurveGauge} from "../actions/withdrawCurveGauge";
-import {CRV_DUO_USDC_crvUSD, CRV_DUO_USDe_USDC, CRV_DUO_USDT_crvUSD} from "defi-resources/build/ressources/lps/curve";
-import {SDT_crvUSD_USDC_STRAT, SDT_crvUSD_USDT_GAUGE, SDT_crvUSD_USDT_STRAT} from "defi-resources/build/ressources/erc20/stakeDao";
-import {CURVE_USDe_USDC_GAUGE} from "defi-resources/build/ressources/erc20/curve";
-import {CRV_USD_LLAMA_VAULT} from "defi-resources/build/ressources/erc20/llamalend";
-import {PointsContext} from "../contexts/PointsContext";
 import {crvUSD} from "defi-resources/build/ressources/erc20/common";
+import {PointsContext} from "../contexts/PointsContext";
+import {mappingMethod} from "./userActionGenerator";
 
 export async function executeGeneratedActions(context: PointsContext): Promise<void> {
     try {
@@ -20,38 +17,38 @@ export async function executeGeneratedActions(context: PointsContext): Promise<v
             throw new Error("Users not initialized. Call initUsers first.");
         }
 
-        const lpToken0 = await depositCurveLP(CRV_DUO_USDe_USDC, BigInt(10 ** 20), BigInt(100000000), user1);
+        const lpToken0 = await depositCurveLP(mappingMethod("USDe_USDC", "depositCurveLP"), BigInt(10 ** 20), BigInt(100000000), user1);
 
-        await depositCurveGauge(CURVE_USDe_USDC_GAUGE, lpToken0, user1, BigInt(100000000));
+        await depositCurveGauge(mappingMethod("USDe_USDC", "depositCurveGauge"), lpToken0, user1, BigInt(100000000));
 
         await context.advanceTime(12);
 
-        await withdrawCurveGauge(CURVE_USDe_USDC_GAUGE, user1, BigInt(1000000));
+        await withdrawCurveGauge(mappingMethod("USDe_USDC", "withdrawCurveGauge"), user1, BigInt(1000000));
 
-        await context.transferPosition(CRV_DUO_USDe_USDC, user1, user2.address, BigInt(5000000));
+        await context.transferPosition(mappingMethod("USDe_USDC", "transferPosition"), user1, user2.address, BigInt(5000000));
 
-        const lpToken1 = await depositCurveLP(CRV_DUO_USDC_crvUSD, BigInt(1000000), BigInt(1e19), user1);
+        const lpToken1 = await depositCurveLP(mappingMethod("USDC_crvUSD", "depositCurveLP"), BigInt(1000000), BigInt(1e19), user1);
 
-        await depositStakeDao(lpToken1, SDT_crvUSD_USDC_STRAT, user1, BigInt(1000000));
-
-        await context.advanceTime(30);
-
-        const lpToken2 = await depositCurveLP(CRV_DUO_USDT_crvUSD, BigInt(10000000), BigInt(1e19), user2);
-
-        await depositStakeDao(lpToken2, SDT_crvUSD_USDT_STRAT, user2, BigInt(1e18));
-
-        await withdrawStakeDao(SDT_crvUSD_USDT_STRAT, user2, BigInt(500000));
-
-        await context.transferPosition(SDT_crvUSD_USDT_GAUGE, user2, user1.address, BigInt(1000000));
-
-        await depositLlamaLend(CRV_USD_LLAMA_VAULT, crvUSD, user2, 1000000, BigInt(1000000));
+        await depositStakeDao(lpToken1, mappingMethod("USDC_crvUSD", "depositStakeDao"), user1, BigInt(1000000));
 
         await context.advanceTime(30);
 
-        await withdrawStakeDao(SDT_crvUSD_USDT_STRAT, user2, BigInt(2000000));
+        const lpToken2 = await depositCurveLP(mappingMethod("USDT_crvUSD", "depositCurveLP"), BigInt(10000000), BigInt(1e19), user2);
+
+        await depositStakeDao(lpToken2, mappingMethod("USDT_crvUSD", "depositStakeDao"), user2, BigInt(1e18));
+
+        await withdrawStakeDao(mappingMethod("USDT_crvUSD", "withdrawStakeDao"), user2, BigInt(500000));
+
+        await context.transferPosition(mappingMethod("USDT_crvUSD", "transferPosition"), user2, user1.address, BigInt(1000000));
+
+        await depositLlamaLend(mappingMethod("CRV_USD", "depositLlamaLend"), crvUSD, user2, 1000000, BigInt(1000000));
+
+        await context.advanceTime(30);
+
+        await withdrawStakeDao(mappingMethod("USDT_crvUSD", "withdrawStakeDao"), user2, BigInt(2000000));
 
         const lpBalance = await lpToken2.balanceOf(user2);
-        await withdrawCurveLP(CRV_DUO_USDT_crvUSD, lpBalance, user2);
+        await withdrawCurveLP(mappingMethod("USDT_crvUSD", "withdrawCurveLP"), lpBalance, user2);
     } catch (error) {
         throw error;
     }
--- js-scripts/hardhat/USG/scripts/userActionGenerator.ts
+++ js-scripts/hardhat/USG/scripts/userActionGenerator.ts
@@ -1,9 +1,8 @@
+import {writeFileSync} from "fs";
 import {CRV_DUO_USDC_crvUSD, CRV_DUO_USDe_USDC, CRV_DUO_USDT_crvUSD} from "defi-resources/build/ressources/lps/curve";
 import {SDT_crvUSD_USDC_STRAT, SDT_crvUSD_USDT_GAUGE, SDT_crvUSD_USDT_STRAT} from "defi-resources/build/ressources/erc20/stakeDao";
 import {CURVE_USDe_USDC_GAUGE} from "defi-resources/build/ressources/erc20/curve";
 import {CRV_USD_LLAMA_VAULT} from "defi-resources/build/ressources/erc20/llamalend";
-import {crvUSD} from "defi-resources/build/ressources/erc20/common";
-import {writeFileSync} from "fs";
 
 interface ActionRow {
     actionType: string;
@@ -11,6 +10,72 @@ interface ActionRow {
     user: string;
     amount: string;
     additionalParams: string;
+}
+
+/**
+ *
+ * @param key ADD ALL POOLS/GAUGE/VAULTS NECESSARY
+ * TO COVER AS MANY USE CASES AS POSSIBLE
+ * @param actionType
+ * @returns
+ */
+export function mappingMethod(key: string, actionType: string): string {
+    switch (actionType.toLowerCase()) {
+        case "depositcurvelp":
+            switch (key) {
+                case "USDe_USDC":
+                    return CRV_DUO_USDe_USDC;
+                case "USDC_crvUSD":
+                    return CRV_DUO_USDC_crvUSD;
+                case "USDT_crvUSD":
+                    return CRV_DUO_USDT_crvUSD;
+                default:
+                    throw new Error(`Unknown key ${key} for depositCurveLP`);
+            }
+        case "depositcurvegauge":
+        case "withdrawcurvegauge":
+            switch (key) {
+                case "USDe_USDC":
+                    return CURVE_USDe_USDC_GAUGE;
+                default:
+                    throw new Error(`Unknown key ${key} for depositCurveGauge/withdrawCurveGauge`);
+            }
+        case "depositstakedao":
+        case "withdrawstakedao":
+            switch (key) {
+                case "USDC_crvUSD":
+                    return SDT_crvUSD_USDC_STRAT;
+                case "USDT_crvUSD":
+                    return SDT_crvUSD_USDT_STRAT;
+                default:
+                    throw new Error(`Unknown key ${key} for depositStakeDao/withdrawStakeDao`);
+            }
+        case "transferposition":
+            switch (key) {
+                case "USDe_USDC":
+                    return CRV_DUO_USDe_USDC;
+                case "USDT_crvUSD":
+                    return SDT_crvUSD_USDT_GAUGE;
+                default:
+                    throw new Error(`Unknown key ${key} for transferPosition`);
+            }
+        case "depositllamalend":
+            switch (key) {
+                case "CRV_USD":
+                    return CRV_USD_LLAMA_VAULT;
+                default:
+                    throw new Error(`Unknown key ${key} for depositLlamaLend`);
+            }
+        case "withdrawcurvelp":
+            switch (key) {
+                case "USDT_crvUSD":
+                    return CRV_DUO_USDT_crvUSD;
+                default:
+                    throw new Error(`Unknown key ${key} for withdrawCurveLP`);
+            }
+        default:
+            throw new Error(`Unknown action type ${actionType} in mappingMethod`);
+    }
 }
 
 class BlockchainScriptGenerator {
@@ -36,6 +101,12 @@ class BlockchainScriptGenerator {
 
     parseCsv(csvData: string): ActionRow[] {
         const rows = csvData.split("\n").map((row) => row.split(","));
+        const headers = rows[0];
+
+        const expectedHeaders = ["Action Type", "Contract Address", "User", "Amount", "Additional Params", "Comment"];
+        if (!headers.every((h, i) => h.trim() === expectedHeaders[i])) {
+            throw new Error("Invalid CSV headers");
+        }
 
         return rows
             .slice(1)
@@ -58,53 +129,58 @@ class BlockchainScriptGenerator {
 
     generateActionCode(row: ActionRow): string {
         const amount = row.amount && !["lpBalance"].includes(row.amount) ? `BigInt(${row.amount})` : row.amount;
+        const contractAddress = row.contractAddress ? `mappingMethod("${row.contractAddress}", "${row.actionType}")` : '""';
 
         switch (row.actionType.toLowerCase()) {
             case "depositcurvelp": {
                 const variableName = this.generateVariableName(row.contractAddress);
                 return `
-                    const ${variableName} = await depositCurveLP(${row.contractAddress}, ${row.additionalParams ? `BigInt(${row.additionalParams})` : "0n"}, ${amount}, ${
-                    row.user
-                });
+                    const ${variableName} = await depositCurveLP(${contractAddress}, ${row.additionalParams ? `BigInt(${row.additionalParams})` : "0n"}, ${amount}, ${row.user});
                 `;
             }
             case "depositcurvegauge": {
-                const lpVariable = this.latestLpVariable || "lpToken";
+                if (!this.latestLpVariable) {
+                    throw new Error(`No LP token variable available for depositCurveGauge with key ${row.contractAddress}`);
+                }
                 return `
-                    await depositCurveGauge(${row.contractAddress}, ${lpVariable}, ${row.user}, ${amount});
+                    await depositCurveGauge(${contractAddress}, ${this.latestLpVariable}, ${row.user}, ${amount});
                 `;
             }
             case "withdrawcurvegauge":
                 return `
-                    await withdrawCurveGauge(${row.contractAddress}, ${row.user}, ${amount});
+                    await withdrawCurveGauge(${contractAddress}, ${row.user}, ${amount});
                 `;
             case "transferposition":
                 return `
-                    await context.transferPosition(${row.contractAddress}, ${row.user}, ${row.additionalParams}.address, ${amount});
+                    await context.transferPosition(${contractAddress}, ${row.user}, ${row.additionalParams ? `${row.additionalParams}.address` : '""'}, ${amount});
                 `;
             case "advancetime":
                 return `
                     await context.advanceTime(${row.amount});
                 `;
             case "depositstakedao": {
-                const lpVariable = this.latestLpVariable || "lpToken";
+                if (!this.latestLpVariable) {
+                    throw new Error(`No LP token variable available for depositStakeDao with key ${row.contractAddress}`);
+                }
                 return `
-                    await depositStakeDao(${lpVariable}, ${row.contractAddress}, ${row.user}, ${amount});
+                    await depositStakeDao(${this.latestLpVariable}, ${contractAddress}, ${row.user}, ${amount});
                 `;
             }
             case "withdrawstakedao":
                 return `
-                    await withdrawStakeDao(${row.contractAddress}, ${row.user}, ${amount});
+                    await withdrawStakeDao(${contractAddress}, ${row.user}, ${amount});
                 `;
             case "depositllamalend":
                 return `
-                    await depositLlamaLend(${row.contractAddress}, ${row.additionalParams}, ${row.user}, ${row.amount}, ${amount});
+                    await depositLlamaLend(${contractAddress}, ${row.additionalParams ? row.additionalParams : '""'}, ${row.user}, ${row.amount}, ${amount});
                 `;
             case "withdrawcurvelp": {
-                const lpVariable = this.latestLpVariable || "lpToken";
+                if (!this.latestLpVariable) {
+                    throw new Error(`No LP token variable available for withdrawCurveLP with key ${row.contractAddress}`);
+                }
                 return `
-                    const lpBalance = await ${lpVariable}.balanceOf(${row.user});
-                    await withdrawCurveLP(${row.contractAddress}, lpBalance, ${row.user});
+                    const lpBalance = await ${this.latestLpVariable}.balanceOf(${row.user});
+                    await withdrawCurveLP(${contractAddress}, lpBalance, ${row.user});
                 `;
             }
             default:
@@ -114,14 +190,16 @@ class BlockchainScriptGenerator {
 
     generateScriptContent(actions: ActionRow[]): string {
         return `
-import {depositCurveLP} from "../actions/depositCurveLP";
-import {depositStakeDao} from "../actions/depositStakeDao";
-import {depositLlamaLend} from "../actions/depositLlamaLend";
-import {withdrawCurveLP} from "../actions/withdrawCurveLP";
-import {withdrawStakeDao} from "../actions/withdrawStakeDao";
-import {depositCurveGauge} from "../actions/depositCurveGauge";
-import {withdrawCurveGauge} from "../actions/withdrawCurveGauge";
-import {PointsContext} from "../contexts/PointsContext";
+import { depositCurveLP } from '../actions/depositCurveLP';
+import { depositStakeDao } from '../actions/depositStakeDao';
+import { depositLlamaLend } from '../actions/depositLlamaLend';
+import { withdrawCurveLP } from '../actions/withdrawCurveLP';
+import { withdrawStakeDao } from '../actions/withdrawStakeDao';
+import { depositCurveGauge } from '../actions/depositCurveGauge';
+import { withdrawCurveGauge } from '../actions/withdrawCurveGauge';
+import { crvUSD } from 'defi-resources/build/ressources/erc20/common';
+import { PointsContext } from '../contexts/PointsContext';
+import { mappingMethod } from './userActionGenerator';
 
 export async function executeGeneratedActions(context: PointsContext): Promise<void> {
     try {
@@ -139,6 +217,8 @@ ${actions.map((action) => this.generateActionCode(action)).join("\n\n")}
     }
 
     async generateScript(): Promise<void> {
+        console.log("CALL generateScript");
+
         try {
             const csvData = await this.getCsv();
             const actions = this.parseCsv(csvData);
@@ -158,6 +238,8 @@ ${actions.map((action) => this.generateActionCode(action)).join("\n\n")}
 }
 
 async function main() {
+    console.log("CALL MAIN IN userActionGenerator.ts");
+
     const generator = new BlockchainScriptGenerator("14kY9R4FzMriJ_vo-rt5yISM0pFIMJdkklcEf-wAlG70", "0");
     await generator.generateScript();
 }
