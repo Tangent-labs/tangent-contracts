f9f2bb38699bb6db387101e580ba4f73469e6337
Ya-Je <95443605+Ya-Je@users.noreply.github.com>
2025-10-14 11:15:53 +0200

    feat: booster setup & Pointprice chainview (#108)
    * feat: booster setup & Pointprice chainview
    
    * feat: addtimstamp
    
    * feat: review
    
    ---------
    
    Co-authored-by: yann-j-nomiks <yannj@zohomail.com>

---
+++ test/unit/chainview/PointPricesChainview.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "../../contexts/MarketDeploymentContext.sol";
+import "../../../src/chainview/USG/bot/PointPrices.cv.sol";
+
+import {console} from "forge-std/console.sol";
+
+contract PointPricesChainviewTest is MarketDeploymentContext {
+    address public constant sCRVUSD = 0x0655977FEb2f289A4aB78af67BAB0d17aAb84367;
+
+    PointPrices public pointPrices;
+
+    function test_point_prices_returns() public {
+        address[] memory erc4626s = new address[](1);
+        erc4626s[0] = sCRVUSD;
+
+        address[] memory pegKeepers = new address[](2);
+        pegKeepers[0] = address(pegKeeperUSG_USDC);
+        pegKeepers[1] = address(pegKeeperUSG_frxUSD);
+
+        PointPrices.AddressesInput memory addresses = PointPrices.AddressesInput({usg: address(usg), usgOracle: address(USGOracle), sUsg: address(sUSG), pegKeepers: pegKeepers});
+
+        MarketExternalActions m1 = deployConvexCurveLPMarket(AddrCurveStableLP.USDC_crvUSD, true);
+        MarketExternalActions m2 = deployConvexFxnLPMarket(AddrCurveStableLP.USDC_fxUSD);
+
+        address[] memory markets = new address[](2);
+        markets[0] = address(m1);
+        markets[1] = address(m2);
+
+        try new PointPrices(erc4626s, addresses, markets) {} catch (bytes memory reason) {
+            console.logBytes(reason);
+            assertTrue(reason.length > 3, "Chainview failed");
+        }
+    }
+}
+
+/*
+ MarketExternalActions[] public markets;
+
+    BorrowInvariantHandler public borrowInvariantHandler;
+    function setUp() public {
+        markets.push(deployConvexCurveLPMarket(AddrCurveStableLP.USDC_crvUSD, true));
+        markets.push(deployConvexCurveLPMarket(AddrCurveStableLP.USDT_crvUSD, true));
+        markets.push(deployConvexFxnLPMarket(AddrCurveStableLP.USDC_fxUSD));*/
