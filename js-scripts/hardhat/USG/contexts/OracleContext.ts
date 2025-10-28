97b9bcb7a1bc4a7aca6a82d657b43ec6693df36e
walk-on-me <rabifromthenight@gmail.com>
2025-10-28 09:49:51 +0100

    Fix deployment script

--- js-scripts/hardhat/USG/contexts/OracleContext.ts
+++ js-scripts/hardhat/USG/contexts/OracleContext.ts
@@ -120,7 +120,7 @@ export class OracleContext {
         const OraclePendlePTFactory = await ethers.getContractFactory("OraclePendlePT");
         for (let index = 0; index < this.oraclePendlePTParams.length; index++) {
             const item = this.oraclePendlePTParams[index];
-            this.oracles[item.key] = (await OraclePendlePTFactory.deploy(PendlePools[item.key].MARKET, this.oracles[item.underlyingOracle], 900)) as unknown as IPriceOracle;
+            this.oracles[item.key] = (await OraclePendlePTFactory.deploy(PendlePools[item.key].MARKET, this.oracles[item.underlyingOracle], 900, 18)) as unknown as IPriceOracle;
         }
     }
 
