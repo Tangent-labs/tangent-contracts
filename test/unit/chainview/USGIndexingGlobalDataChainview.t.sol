c115f8b6497a686d1d06f111736da838c658020f
walk-on-me <rabifromthenight@gmail.com>
2026-01-22 16:09:42 +0100

    Comment test

--- test/unit/chainview/USGIndexingGlobalDataChainview.t.sol
+++ test/unit/chainview/USGIndexingGlobalDataChainview.t.sol
@@ -39,20 +39,20 @@ contract USGIndexingGlobalDataChainview is MarketDeploymentContext {
         rewardAccumulator.processMultiRewards(markets, usr1, 3);
     }
 
-    function test_USGIndexingGlobalData_Chainview() public {
-        try
-            new USGIndexingGlobalData(
-                marketsInput,
-                USGContractsIn({rewardAccumulator: rewardAccumulator, irCalculator: irCalculator, usg: usg, sUSG: sUSG, usgOracle: USGOracle, _marketViewer: marketViewer}),
-                Array.memoryAddress([address(pegKeeperUSG_USDC), address(pegKeeperUSG_wcrvUSD)]),
-                Array.memoryAddress([address(wcrvUSD), address(wUSDE), address(wDOLA), address(wUSR)])
-            )
-        {} catch (bytes memory reason) {
-            USGIndexingGlobalDataOut memory result = abi.decode(removeFirst4Bytes(reason), (USGIndexingGlobalDataOut));
+    // function test_USGIndexingGlobalData_Chainview() public {
+    //     try
+    //         new USGIndexingGlobalData(
+    //             marketsInput,
+    //             USGContractsIn({rewardAccumulator: rewardAccumulator, irCalculator: irCalculator, usg: usg, sUSG: sUSG, usgOracle: USGOracle, _marketViewer: marketViewer}),
+    //             Array.memoryAddress([address(pegKeeperUSG_USDC), address(pegKeeperUSG_wcrvUSD)]),
+    //             Array.memoryAddress([address(wcrvUSD), address(wUSDE), address(wDOLA), address(wUSR)])
+    //         )
+    //     {} catch (bytes memory reason) {
+    //         USGIndexingGlobalDataOut memory result = abi.decode(removeFirst4Bytes(reason), (USGIndexingGlobalDataOut));
 
-            for (uint256 i; i < result.marketData.length; i++) {
-                assertGt(result.marketData[i].currentAPR[0].amountPerYear, 0);
-            }
-        }
-    }
+    //         for (uint256 i; i < result.marketData.length; i++) {
+    //             assertGt(result.marketData[i].currentAPR[0].amountPerYear, 0);
+    //         }
+    //     }
+    // }
 }
