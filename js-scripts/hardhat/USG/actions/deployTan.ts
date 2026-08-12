ce75244f29caa5c58c643d4fc4369f8a01e24c77
Trottoir <maxime.ranciere@2018.icam.fr>
2026-08-12 14:36:52 +0200

    Add TAN sending to all test users

--- js-scripts/hardhat/USG/actions/deployTan.ts
+++ js-scripts/hardhat/USG/actions/deployTan.ts
@@ -1,6 +1,7 @@
 import { ethers } from "hardhat"
 import { PROD_ADDRESSES } from "../../../../ignition/prod_addresses"
 import { parseEther } from "ethers"
+import { impersonateAccount } from "@nomicfoundation/hardhat-toolbox/network-helpers"
 
 export async function deployTan() {
     const tan = await ethers.deployContract("TAN", [PROD_ADDRESSES.DAO])
@@ -8,6 +9,19 @@ export async function deployTan() {
 
     console.log("TAN", await tan.getAddress())
     console.log("vsTAN", await vsTAN.getAddress())
+    const signers = await ethers.getSigners()
+    const dao = await ethers.getSigner(PROD_ADDRESSES.DAO)
+
+    const tx = await signers[1].sendTransaction({
+        to: dao,
+        value: ethers.parseEther("1"),
+    });
+    await impersonateAccount(PROD_ADDRESSES.DAO)
+
+    for (let index = 0; index < signers.length; index++) {
+        const signer = signers[index];
+        await tan.connect(dao).transfer(signer, parseEther("10000"))
+    }
 
 }
 
