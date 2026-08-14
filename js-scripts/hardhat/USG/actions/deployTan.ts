06e73da1b5967ab6ec9b651376be4de786f41eef
Trottoir <maxime.ranciere@2018.icam.fr>
2026-08-14 12:11:32 +0200

    Add deploy of TAN/WETH LP

--- js-scripts/hardhat/USG/actions/deployTan.ts
+++ js-scripts/hardhat/USG/actions/deployTan.ts
@@ -1,7 +1,9 @@
 import { ethers } from "hardhat"
 import { PROD_ADDRESSES } from "../../../../ignition/prod_addresses"
-import { parseEther } from "ethers"
-import { impersonateAccount } from "@nomicfoundation/hardhat-toolbox/network-helpers"
+import { MaxUint256, parseEther, parseUnits, Signer } from "ethers"
+import { impersonateAccount, stopImpersonatingAccount } from "@nomicfoundation/hardhat-toolbox/network-helpers"
+import { COMMON_ERC20S } from "@tangent/defi-resources"
+import { giveTokenToAddress } from "../../thief/thief"
 
 export async function deployTan() {
     const tan = await ethers.deployContract("TAN", [PROD_ADDRESSES.DAO])
@@ -12,17 +14,56 @@ export async function deployTan() {
     const signers = await ethers.getSigners()
     const dao = await ethers.getSigner(PROD_ADDRESSES.DAO)
 
-    const tx = await signers[1].sendTransaction({
+    await signers[1].sendTransaction({
         to: dao,
         value: ethers.parseEther("1"),
     });
     await impersonateAccount(PROD_ADDRESSES.DAO)
-
-    for (let index = 0; index < signers.length; index++) {
+    for (let index = 0; index < signers.length - 10; index++) {
         const signer = signers[index];
-        await tan.connect(dao).transfer(signer, parseEther("10000"))
+        await tan.connect(dao).transfer(signer, parseEther("333000"))
     }
 
+    await stopImpersonatingAccount(PROD_ADDRESSES.DAO)
+
+    const lp = await deploy_TAN_ETH_LP(await tan.getAddress(), signers[0])
+    console.log("LP TAN/WETH", await lp.getAddress())
+
+}
+
+async function deploy_TAN_ETH_LP(tan: string, owner: Signer) {
+
+    const curveStableSwapFactory = await ethers.getContractAt("ICurveCryptoSwapFactoryNG", "0x98EE851a00abeE0d95D08cF4CA2BdCE32aeaAF7F");
+
+    const poolCount = await curveStableSwapFactory.pool_count();
+    const lpCreationTx = await curveStableSwapFactory
+        .deploy_pool(
+            "TAN",
+            "TAN",
+            [COMMON_ERC20S.WETH, tan],
+            0,
+            400000,
+            145000000000000,
+            26000000,
+            45000000,
+            230000000000000,
+            2000000000000,
+            146000000000000,
+            866,
+            6006006006000
+        );
+    await lpCreationTx.wait();
+
+    await giveTokenToAddress(owner, "WETH", parseEther("400"))
+    const lp = await ethers.getContractAt("ICurveCryptoSwap", await curveStableSwapFactory.pool_list(poolCount));
+    const coin0 = await ethers.getContractAt("ERC20", COMMON_ERC20S.WETH);
+    const coin1 = await ethers.getContractAt("ERC20", tan);
+    await coin0.connect(owner).approve(lp, MaxUint256);
+    await coin1.connect(owner).approve(lp, MaxUint256);
+
+    await lp.connect(owner)["add_liquidity(uint256[2],uint256)"]([parseUnits("200", await coin0.decimals()), parseUnits("333000", await coin1.decimals())], 0);
+
+    return lp;
 }
 
 deployTan()
